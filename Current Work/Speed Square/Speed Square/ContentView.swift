5566b24bd8319db3d6cd46ffad91b85bf8aa8c31
Steven Grutman <sgrutman978@gmail.com>
2019-10-05 17:06:58 -0400

    reorganize, revamp start

---
+++ Current Work/Speed Square/Speed Square/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speed Square
+//
+//  Created by Steven Grutman on 10/5/19.
+//  Copyright © 2019 Steven Grutman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
